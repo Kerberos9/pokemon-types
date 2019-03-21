3f09fdc8aa746982b1a78724e12d6ba862c855f6
Kerberos9 <mll.kerberos@gmail.com>
2019-03-21 11:23:35 +0100

    Fixed all selectors and fixed pokemon list

--- src/components/Type/Type.js
+++ src/components/Type/Type.js
@@ -10,42 +10,36 @@ class Type extends Component {
   };
 
   componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log(prevProps, this.props);
-    if (
-      prevProps.selected[0] === this.props.selected[0] &&
-      prevProps.selected[1] === this.props.selected[1]
-    ) {
-      return;
+    let result = 0;
+    let damageClass = 'default';
+    if (this.props.selected[0] === this.props.selected[1]) {
+      result += this.props.stats[this.props.selected[0]];
     } else {
-      let result = 0;
-      let damageClass = 'default';
-      if (this.props.selected[0] === this.props.selected[1]) {
-        result += this.props.stats[this.props.selected[0]];
-      } else {
-        result +=
-          this.props.stats[this.props.selected[0]] +
-          this.props.stats[this.props.selected[1]];
-      }
-      switch (result) {
-        case -2:
-          damageClass = 'quarter';
-          break;
-        case -1:
-          damageClass = 'half';
-          break;
-        case 0:
-          damageClass = 'default';
-          break;
-        case 1:
-          damageClass = 'double';
-          break;
-        case 2:
-          damageClass = 'quad';
-          break;
-        default:
-          damageClass = 'immune';
-          break;
-      }
+      result +=
+        this.props.stats[this.props.selected[0]] +
+        this.props.stats[this.props.selected[1]];
+    }
+    switch (result) {
+      case -2:
+        damageClass = 'quarter';
+        break;
+      case -1:
+        damageClass = 'half';
+        break;
+      case 0:
+        damageClass = 'default';
+        break;
+      case 1:
+        damageClass = 'double';
+        break;
+      case 2:
+        damageClass = 'quad';
+        break;
+      default:
+        damageClass = 'immune';
+        break;
+    }
+    if (damageClass !== this.state.damageClass) {
       this.setState({ damageClass, value: result });
     }
   }
@@ -53,7 +47,7 @@ class Type extends Component {
   render() {
     return (
       <div className={this.state ? this.state.damageClass : ''}>
-        <p>{this.capitalizeFirstLetter(this.props.type) + this.state.value}</p>
+        <p>{this.capitalizeFirstLetter(this.props.type)}</p>
       </div>
     );
   }
